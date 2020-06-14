4e6b4ea8289758aa2275f68c09af2419263a4716
Samuel S. Watson <samuel.s.watson@gmail.com>
2020-06-14 13:29:01 -0400

    Accommodate mathematical formulas

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 module.exports = function toPlaintext(delta) {
   return delta.reduce(function (text, op) {
-    if (!op.insert) throw new TypeError('only `insert` operations can be transformed!');
-    if (typeof op.insert !== 'string') return text + ' ';
+    if (!op.insert) {
+        throw new TypeError('only `insert` operations can be transformed!');
+    }
+    if (typeof op.insert === 'object' && op.insert.formula) {
+        return text + op.insert.formula;
+    }
+    if (typeof op.insert !== 'string') {
+        return text + ' ';
+    }
     return text + op.insert;
   }, '');
 };
